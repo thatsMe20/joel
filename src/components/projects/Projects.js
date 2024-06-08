6b45b2061d4a619e01fe762bb38bf21986cd2a3a
thatsMe20 <joel.abunales20@gmail.com>
2024-06-08 09:13:46 +0800

    changed

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import Title from '../layouts/Title'
+import { projectOne, projectTwo, projectThree, projectFour, projectFive, projectSix } from "../../assets/index";
+import ProjectsCard from './ProjectsCard';
+
+const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="w-full py-20 border-b-[1px] border-b-black"
+    >
+      <div className="flex justify-center items-center text-center">
+        <Title
+          title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
+          des="My Projects"
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
+        <ProjectsCard
+          title="Recruitment for HR (Dynamic Website)"
+          des="The recruitment team's goal is to locate and employ competent applicants 
+          to cover unfilled roles in the company. This entails being aware of the staffing 
+          requirements of different teams or departments and making sure that openings 
+          are filled promptly to support business operations."
+          src={projectOne}
+        />
+        <ProjectsCard
+          title="Shoppee Poke (Send Bulk Email & SMS) Dynamic System"
+          des="The Shoppee poke was created to track every user's action and send mass emails and messages to the customers."
+          src={projectTwo}
+        />
+        <ProjectsCard
+          title="HRIS (Dynamic Website)"
+          des="HRIS, which stands for Human Resource Information System, 
+          is a software solution used by organizations to manage and streamline 
+          various human resource functions and processes. It serves as a 
+          centralized database for storing employee information"
+          src={projectThree}
+        />
+        <ProjectsCard
+          title="BPO Automation for Invoice"
+          des="Confidential"
+          src={projectFour}
+        />
+        <ProjectsCard
+          title="DX Infohub(Dynamic Website)"
+          des="BPO Infohub was created to update teams for upcoming events. 
+          And for new hires to read important documents also to see all team members."
+          src={projectFive}
+        />
+        <ProjectsCard
+          title="Zonta (Dynamic Website)"
+          des="Zonta International is a global organization dedicated to empowering women 
+          through service and advocacy. Founded in 1919 in Buffalo, New York, Zonta aims 
+          to improve the legal, political, economic, educational, health, and professional 
+          status of women worldwide."
+          src={projectSix}
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects
---
+++ src/components/resume/Education.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { motion } from "framer-motion";
+import EducationCard from "./EducationCard";
+
+const Education = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.5 } }}
+      className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
+    >
+      {/* part one */}
+      <div>
+        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]">2014 - 2018</p>
+          <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
+        </div>
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <EducationCard
+            title="BS in Information Technology"
+            subTitle="Access Computer College (2014 - 2018)"
+            des="The program covers various areas of IT, including software development, networking, database management, web development, and information security."
+          />
+          {/* <ResumeCard
+            title="Secondary School Education"
+            subTitle="Kingstar Secondary School (1998 - 2000)"
+            result="5.00/5"
+            des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
+          /> */}
+        </div>
+      </div>
+      {/* part Two */}
+
+      <div>
+        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]">2018 - Present</p>
+          <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
+        </div>  
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <EducationCard
+            title="Sr. RPA Developer & Sr. Fullstack Developer"
+            subTitle="Robotic Process Automation & Fullstack Developer - (2019 - Present)"
+            result="transcosmos Asia Philippines"
+            des="I automate repetitive, rule-based tasks such as data entry, form processing, and routine calculations. This reduces the workload on clients and minimizes errors.
+            and Developed a web-based for prospective employees (TAR & HR Website), a team portfolio (Team Infohub), Emailing and sending bulk 
+            messages (Shoppee Poke) and DTR concerning external personnel"
+          />
+          <EducationCard
+            title="Freelancer"
+            subTitle="Apple Developer Team - (2023)"
+            result="Philippines"
+            des="Update front-end development and improve some features.
+            Created an Client and product management (Lands Upholstery Shop & Raianseeir Team)"
+          />
+          <EducationCard
+            title="Jr. Web Developer"
+            subTitle="Fullstack Developer - ( 2018- 2019)"
+            result="DIGITS inc."
+            des="Create test strategies and reliable quality assessment criteria to determine whether the program satisfies the required standards. Verify that test results reflect what was anticipated, 
+            then perform quality assurance and system debugging."
+          />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Education;
